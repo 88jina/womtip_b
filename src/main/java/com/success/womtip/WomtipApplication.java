3f274fd75a78ae5d08362002d325e7447446162a
88jina <jina940323@gmail.com>
2021-11-29 22:59:42 +0900

    init commit

---
+++ src/main/java/com/success/womtip/WomtipApplication.java
@@ -0,0 +1,13 @@
+package com.success.womtip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomtipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WomtipApplication.class, args);
+    }
+
+}
