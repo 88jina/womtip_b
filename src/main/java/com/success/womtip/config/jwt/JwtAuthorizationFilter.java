a1bcb1be5f5fdc9649590a8b4f759dd114496885
88jina <jina940323@gmail.com>
2022-01-12 03:48:20 +0900

    added jwt authorization filter

--- src/main/java/com/success/womtip/config/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/success/womtip/config/jwt/JwtAuthorizationFilter.java
@@ -1,4 +1,58 @@
 package com.success.womtip.config.jwt;
 
-public class JwtAuthorizationFilter {
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.success.womtip.config.auth.PrincipalDetails;
+import com.success.womtip.entity.Member;
+import com.success.womtip.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private MemberRepository memberRepository;
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, MemberRepository memberRepository) {
+        super(authenticationManager);
+        this.memberRepository = memberRepository;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String hearder = request.getHeader(JwtProperties.HEADER_STRING);
+        if (hearder == null || !hearder.startsWith(JwtProperties.TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        log.debug("HEADER >>>>>>> " + hearder);
+
+        String token = request.getHeader(JwtProperties.HEADER_STRING).replace(JwtProperties.TOKEN_PREFIX, "");
+
+        String memberId = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(token).getClaim("memberId").asString();
+
+        if (memberId != null) {
+            Member member = memberRepository.findMemberByMemberId(memberId);
+
+            PrincipalDetails principalDetails = new PrincipalDetails(member);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(principalDetails, principalDetails.getAuthorities());
+
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        chain.doFilter(request, response);
+
+    }
 }
